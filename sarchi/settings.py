8e9a68b98758e9fde8623595ed0e6ab95b928798
edmundrotimi <edmundrotimi@gmail.com>
2023-08-10 18:32:40 +0200

    add jazzmin to installed apps

--- sarchi/settings.py
+++ sarchi/settings.py
@@ -24,6 +24,9 @@ ADMIN_PATH = config('ADMIN_PATH')
 
 # Application definition
 INSTALLED_APPS = [
+    # third party apps
+    'jazzmin',
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
