dc43cfbccd4a158d46e93851dda628efca48f163
edmundrotimi <edmundrotimi@gmail.com>
2023-08-10 17:38:17 +0200

    add django-debug-toolbar’s to project url

--- sarchi/urls.py
+++ sarchi/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path(f'{settings.ADMIN_PATH}/', admin.site.urls),
 ]
+
+
+# add admin path to urlpatterns
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
