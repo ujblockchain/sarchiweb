39627cfbd3b2eba30a2fb728d03538dd841055ee
edmundrotimi <edmundrotimi@gmail.com>
2023-08-10 15:34:44 +0200

    set admin path

--- sarchi/urls.py
+++ sarchi/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{settings.ADMIN_PATH}/', admin.site.urls),
 ]
