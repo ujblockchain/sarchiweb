38f26e6af2277ca994a634e3c03b35a001d5f7d8
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-18 15:04:12 +0200

    pages app test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
+from django.db.models import signals
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PagesViewTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # disable any active signals
+        signals.post_save.receivers = []
+        signals.pre_save.receivers = []
+
+    def test_home_page_views(self):
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.charset, 'utf-8')
+        self.assertTrue('Blockchain' in str(response.content))
+        self.assertTrue('main.js' in str(response.content))
+        self.assertTrue('csrfmiddlewaretoken' in str(response.content))
