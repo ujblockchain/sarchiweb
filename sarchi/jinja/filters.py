4a196fbca66755a23107b8959853b53a55a5a363
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:57:38 +0200

    add filter for dict to string converter

--- sarchi/jinja/filters.py
+++ sarchi/jinja/filters.py
@@ -1,3 +1,4 @@
+import ast
 from datetime import datetime
 from django.utils.timezone import utc
 
@@ -8,3 +9,8 @@ current_timestamp = datetime.now().replace(tzinfo=utc)
 
 def dateformat(value):
     return f'{value.strftime("%B")} {value.strftime("%d")}, {value.strftime("%Y")}'
+
+
+# convert dict to string
+def dict_string(value):
+    return ast.literal_eval(value)
