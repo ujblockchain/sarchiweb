2c74ab2e46dcb4118f14d6081bfdb2eec048c541
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:45:02 +0200

    admin settings for user contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import UserContact
 
-# Register your models here.
+
+class UserContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'email',
+        'phone',
+        'message',
+        'timestamp',
+    ]
+    list_display_links = [
+        'name',
+        'email',
+        'phone',
+        'message',
+    ]
+    search_fields = [
+        'name',
+        'email',
+        'phone',
+        'message',
+        'timestamp',
+    ]
+    readonly_fields = [
+        'name',
+        'email',
+        'phone',
+        'message',
+        'timestamp',
+    ]
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fields = ['name', 'email', 'phone', 'message', 'timestamp']
+
+
+#
+admin.site.register(UserContact, UserContactAdmin)
