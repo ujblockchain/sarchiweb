7245f079665e3c1d069cf848e4bd2c86f686f366
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:55:37 +0200

    test for  user contact form

--- contact/tests.py
+++ contact/tests.py
@@ -49,3 +49,50 @@ class ContactTest(TestCase):
         # check model filed max length
         max_length = UserContact._meta.get_field('name').max_length
         self.assertEqual(max_length, 100)
+
+
+class ContactFormTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # disable signals
+        signals.post_save.receivers = []
+        signals.pre_save.receivers = []
+
+    def test_email_post_form(self):
+        # get response
+        response = self.client.post(
+            reverse('contact'),
+            {
+                'name': 'John Deo',
+                'email': 'johndeo@ujblockchain.co.za',
+                'phone': '+27111111111',
+                'message': 'Hello',
+            },
+        )
+
+        self.assertRedirects(response, '/#contact')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/#contact'))
+
+    def test_send_email_form(self):
+        form = UserMessageForm(
+            data={
+                'name': 'John Deo',
+                'email': 'johndeo@ujblockchain.co.za',
+                'phone': '+27111111111',
+                'message': 'Hello',
+                'captcha': '',
+            }
+        )
+
+        # remove captcha from test form
+        del form.fields['captcha']
+
+        self.assertTrue(form.fields['phone'])
+        self.assertFalse(form.errors)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.fields['name'])
+        self.assertEqual(form.cleaned_data['name'], 'John Deo')
+        self.assertTrue(form.cleaned_data['email'] == 'johndeo@ujblockchain.co.za')
+        self.assertEqual(form.cleaned_data['message'], 'Hello')
+        self.assertEqual(form.fields['email'].help_text, '')
