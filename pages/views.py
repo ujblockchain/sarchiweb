ba4074089628c343fefbcb71740a553b794721b9
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:59:20 +0200

    add contact form to view context data

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from contact.forms import UserMessageForm
 
 
 # index page view
 class HomeView(TemplateView):
     template_name = "pages/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserMessageForm()
+        return context
