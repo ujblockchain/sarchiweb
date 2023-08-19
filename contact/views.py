9a438b6dd4d58caacd6a55e8455f4221d1ab6b61
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:56:11 +0200

    user contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.views.generic import View
+from .models import UserContact
+from .forms import UserMessageForm
 
-# Create your views here.
+
+class ContactView(View):
+    def post(self, request):
+        form = UserMessageForm(request.POST)
+
+        try:
+            # check if form is valid
+            if form.is_valid():
+                # init model instance
+                contact = UserContact(
+                    name=form.cleaned_data['name'],
+                    email=form.cleaned_data['email'],
+                    phone=form.cleaned_data['phone'],
+                    message=form.cleaned_data['message'],
+                )
+
+                # check if error exist in form
+                contact.clean_fields()
+
+                # save form
+                contact.save()
+
+                # sent success message
+                messages.add_message(request, messages.SUCCESS, 'Thank You. Your Message has been Submitted')
+
+                # redirect to form section
+                return HttpResponseRedirect('/#contact')
+            else:
+                # raise exception
+                raise Exception
+
+        except Exception as e:
+            # get the error and send as an error message
+            messages.add_message(request, messages.ERROR, {
+                'form_error': form.errors,
+                'msg': 'Invalid Form Field(s)'
+            })
+
+            # redirect to form section
+            return HttpResponseRedirect('/#contact')
