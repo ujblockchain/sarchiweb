bd282f0922392a3c58ce3a5807926e3918ac2b75
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-19 10:44:20 +0200

    user contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserContact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    message = models.TextField(max_length=800)
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = "User's Contact"
+        verbose_name_plural = "User's Contacts"
