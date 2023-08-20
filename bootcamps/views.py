d75a0e667bd43cbf20c0f4c69b7bacb3c33b72f5
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-20 06:57:55 +0200

    get request for bootcamp view

--- bootcamps/views.py
+++ bootcamps/views.py
@@ -1,3 +1,21 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib import messages
+from django.views.generic import View
 
-# Create your views here.
+from contact.forms import UserMessageForm
+from .forms import BootcampForm
+from .models import BootcampFirst
+
+
+class BootcampView(View):
+    def get(self, request):
+        # set template
+        template_name = 'forms/training-form.html'
+
+        # init context
+        context = {'form': UserMessageForm(), 'training_form': BootcampForm()}
+
+        return render(request, template_name, context)
