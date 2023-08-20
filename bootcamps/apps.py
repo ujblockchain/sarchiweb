f0249919df726c025c10b4dfbca136169e7202ed
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-20 06:39:46 +0200

    register bootcamp signals

--- bootcamps/apps.py
+++ bootcamps/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BootcampsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bootcamps'
+    
+    def ready(self):
+        import bootcamps.signals
