3cb9fa91f923d1e473a5aedd5449ed5799032a11
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-20 06:40:47 +0200

    add bootcamps app model

--- bootcamps/models.py
+++ bootcamps/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+level = (
+    ('Higher Certificate', 'Higher Certificate'),
+    ('Diploma', 'Diploma'),
+    ('Honors', 'Honors'),
+    ('Undergrad', 'Undergrad'),
+    ('Post Graduate', 'Post Graduate'),
+    ('Others', 'Others'),
+)
+
+applicant_selection = (
+    ('Selected', 'Selected'),
+    ('Rejected', 'Rejected'),
+)
+
+
+class BootcampFirst(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    faculty = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    level = models.CharField(max_length=50, choices=level, default='Undergrad')
+    expectation = models.TextField(max_length=800)
+    application_status = models.BooleanField(null=True, blank=True, choices=applicant_selection)
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = "Bootcamp Sep 2023"
+        verbose_name_plural = "Bootcamp Sep 2023"
