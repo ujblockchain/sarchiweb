e49944b7df7f4a27d42f0db3d30c017fe8995fc4
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-20 06:49:21 +0200

    account lockout url path

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from .views import revisiondelete, AccountLockout
 
 urlpatterns = [
     path('revision/delete', revisiondelete, name="rev_delete"),
+    path('user/locked/', AccountLockout.as_view(), name='lockout'),
 ]
