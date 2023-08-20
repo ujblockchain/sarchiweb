e9c5ad0dcf390ea6ce3b2dc2f8fac3484a5d892f
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-20 06:46:04 +0200

    add django revision delete view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+User = get_user_model()
+
+
+# test for only admin
+def test_user_role(request):
+    return User.objects.filter(is_superuser=True)
+
+
+@user_passes_test(test_user_role)
+def revisiondelete(request):
+    if request.method == 'POST':
+        app_name = request.POST.get('app_name')
+        model_name = request.POST.get('model_name')
+        path = request.POST.get('path')
+        model_name = model_name.replace(" ", "")
+        app_name_lower = app_name.lower()
+        # call revision command
+        call_command('deleterevisions', f"{app_name_lower}.{model_name}", days=30)
+        # success message
+        messages.add_message(request, messages.SUCCESS, 'Revision(s) deleted successfully')
+        # redirect to previous page
+        return redirect(path)
