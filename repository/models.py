44be612cb025cc08779242fb71fa10eab0eb3175
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-21 21:12:09 +0200

    repository app model

--- repository/models.py
+++ repository/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class RepoInfo(models.Model):
+    active_repo = models.URLField()
+    total_repo = models.IntegerField()
+    total_commit = models.IntegerField(default=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.active_repo
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = "Repository"
+        verbose_name_plural = "Repository"
