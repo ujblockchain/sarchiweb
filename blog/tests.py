ab97e229d5a2790278f64d2168ace336b78828b9
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-21 14:21:31 +0200

    blog list view test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
+from django.db.models import signals
 from django.test import TestCase
+from django.urls import reverse
+from django.conf import settings
+import shortuuid
+from model_bakery import baker
+from .models import Blog
 
-# Create your tests here.
+
+class PostListViewTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # disable any active signals
+        signals.post_save.receivers = []
+        signals.pre_save.receivers = []
+
+        id = shortuuid.ShortUUID().random(length=16)
+        load_image_1 = f'{settings.PROJECT_DIR}/static/images/logo.png'
+        load_image_2 = f'{settings.PROJECT_DIR}/static/images/main-logo.png'
+        load_image_3 = f'{settings.PROJECT_DIR}/static/images/UJ.png'
+
+        # create model
+        cls.bootcamp = baker.make(
+            Blog,
+            id=id,
+            title='Blockchain Master Class',
+            slug='blockchain_master_class',
+            first_section_image=load_image_1,
+            third_section_image=load_image_2,
+            fifth_section_image=load_image_3,
+        )
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        self.bootcamp.delete()
+
+    def test_blog_list_views(self):
+        response = self.client.get(reverse('blog_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.charset, 'utf-8')
+        self.assertTrue('blockchain' in str(response.content))
+        self.assertTrue('main.css' in str(response.content))
+        self.assertTrue('csrfmiddlewaretoken' in str(response.content))
