3bfe2c0db7dd3bb776e346b5b19e6a2c9fcaef7b
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-21 14:24:54 +0200

    blog list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from datetime import datetime
+from django.utils.timezone import utc
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from contact.forms import UserMessageForm
+from .models import Blog
+
+
+# init current time
+current_timestamp = datetime.now().replace(tzinfo=utc)
+
+
+class PostListViews(ListView):
+    model = Blog
+    queryset = Blog.objects.filter(publish=True, schedule_message__lte=current_timestamp)
+    context_object_name = 'post'
+    template_name = 'blog/blog-list.html'
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserMessageForm()
+        return context
