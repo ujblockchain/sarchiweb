fea3f1694f49895312ba498fc8550952f2a699ca
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-21 14:23:10 +0200

    blog urls path

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostListViews, PostDetailViews
+
+
+urlpatterns = [
+    path('', PostListViews.as_view(), name='blog_list'),
+    path('<str:id>/<slug:slug>', PostDetailViews.as_view(), name='blog_details'),
+]
