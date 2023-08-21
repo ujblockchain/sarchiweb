3196001bba6539b479301a132e0c4afd07222ee2
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-21 21:12:44 +0200

    register repository app model in admin

--- repository/admin.py
+++ repository/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import RepoInfo
 
-# Register your models here.
+
+class RepoAdmin(admin.ModelAdmin):
+    list_display = [
+        'active_repo',
+        'total_repo',
+        'total_commit',
+        'timestamp',
+    ]
+    list_display_links = [
+        'active_repo',
+        'total_repo',
+        'total_commit',
+    ]
+    search_fields = [
+        'active_repo',
+        'total_repo',
+        'total_commit',
+        'timestamp',
+    ]
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fields = [
+        'active_repo',
+        'total_repo',
+        'total_commit',
+        'timestamp',
+    ]
+
+#
+admin.site.register(RepoInfo, RepoAdmin)
