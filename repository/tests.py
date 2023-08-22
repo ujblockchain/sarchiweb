40dd8b35cf333e4d15c4eb0d2cfeef29b41eefb7
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-22 14:54:58 +0200

    repository app test

--- repository/tests.py
+++ repository/tests.py
@@ -1,3 +1,43 @@
+from datetime import datetime
+from django.utils.timezone import utc
+from django.db.models import signals
 from django.test import TestCase
+from decouple import config
+from model_bakery import baker
+from .models import RepoInfo
 
-# Create your tests here.
+
+class RepoTest(TestCase):
+    def setUp(self):
+        # init current time (timezone aware)
+        self.current_timestamp = datetime.now().replace(tzinfo=utc)
+
+    @classmethod
+    def setUpTestData(cls):
+        # disable signals
+        signals.post_save.receivers = []
+        signals.pre_save.receivers = []
+
+        # create model
+        cls.partner = baker.make(
+            RepoInfo,
+            active_repo=config('GITHUB_EVENT_URL'),
+            total_commit=200,
+        )
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        self.partner.delete()
+
+    def test_repo(self):
+        # init model
+        model = self.partner
+
+        self.assertEqual(model.active_repo, config('GITHUB_EVENT_URL'))
+        self.assertTrue('github' in model.active_repo)
+        self.assertEqual(model.clean_fields(), None)
+        self.assertEqual(model.clean(), None)
+        self.assertNotEqual(model.timestamp, self.current_timestamp)
+        # check model filed max length
+        commit_count = RepoInfo._meta.get_field('total_commit').default
+        self.assertEqual(commit_count, 100)
