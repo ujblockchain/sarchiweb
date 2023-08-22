c5dc7f75b05d0569ba546dc55a6d96cbffe13f25
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-22 14:57:31 +0200

    partner model app

--- partners/models.py
+++ partners/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from blog.validate_image import clean_image
 
-# Create your models here.
+
+class Partners(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='partners/logo/', validators=[clean_image])
+    publish = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['date_created']
+        verbose_name = "Partner"
+        verbose_name_plural = "Partners"
