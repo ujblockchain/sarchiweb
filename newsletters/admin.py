b924eebe14a514a42c1c05823730111e8f2f900c
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-23 12:43:54 +0200

    register app model in admin

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import NewsletterEmail
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = [
+        'email',
+        'timestamp',
+    ]
+    list_display_links = [
+        'email',
+        'timestamp',
+    ]
+    search_fields = [
+        'email',
+        'timestamp',
+    ]
+    readonly_fields = [
+        'email',
+        'timestamp',
+    ]
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fields = ['email', 'timestamp']
+
+
+#
+admin.site.register(NewsletterEmail, NewsletterAdmin)
