e7a8e42f9531d9dad047b9e81e7362517f78ec43
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-24 13:52:40 +0200

    add slash

--- bootcamps/urls.py
+++ bootcamps/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BootcampView
 
 urlpatterns = [
-    path('registration', BootcampView.as_view(), name='bootcamp')
+    path('registration/', BootcampView.as_view(), name='bootcamp')
 ]
