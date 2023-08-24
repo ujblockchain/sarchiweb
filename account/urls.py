b78ef86cf72e0a93a99f8d97aff9437b1e72917d
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-08-24 13:54:47 +0200

    add slash to urls

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import revisiondelete, AccountLockout
 
 urlpatterns = [
-    path('revision/delete', revisiondelete, name="rev_delete"),
+    path('revision/delete/', revisiondelete, name="rev_delete"),
     path('user/locked/', AccountLockout.as_view(), name='lockout'),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import PostListViews, PostDetailViews
 
 urlpatterns = [
     path('', PostListViews.as_view(), name='blog_list'),
-    path('<str:id>/<slug:slug>', PostDetailViews.as_view(), name='blog_details'),
+    path('<str:id>/<slug:slug>/', PostDetailViews.as_view(), name='blog_details'),
 ]
