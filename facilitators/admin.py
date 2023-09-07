d7ea7cf0194eb8f089be336cc8d583d5e56e358d
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-07 14:21:05 +0200

    register model in admin

--- facilitators/admin.py
+++ facilitators/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Facilitators
 
-# Register your models here.
+
+class FacilitatorAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'full_name',
+        'role',
+        'phone_number',
+        'image',
+        'publish',
+        'timestamp',
+    ]
+    list_display_links = [
+        'id',
+        'full_name',
+        'role',
+        'phone_number',
+        'image',
+    ]
+    search_fields = [
+        'id',
+        'full_name',
+        'role',
+        'phone_number',
+        'image',
+        'publish',
+        'timestamp',
+    ]
+    readonly_fields = ['id']
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fields = ['id', 'full_name', 'role', 'phone_number', 'image', 'publish', 'timestamp']
+
+
+#
+admin.site.register(Facilitators, FacilitatorAdmin)
