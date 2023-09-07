4988847ca2c3c40ae648d16d5bfc48dfcd96107a
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-07 14:22:17 +0200

    add model id

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from shortuuid.django_fields import ShortUUIDField
 
 
 class NewsletterEmail(models.Model):
+    # A primary key ID of length 16 and a short alphabet.
+    id = ShortUUIDField(
+        length=16,
+        max_length=40,
+        alphabet="abcdefg1234",
+        primary_key=True,
+    )
     email = models.EmailField()
     date_created = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(default=timezone.now)
