85b71f1040fb32f321678bff98348bc479898640
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-07 14:32:50 +0200

    update newsletter app urls

--- newsletters/urls.py
+++ newsletters/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import NewsletterView
+from .views import (
+    NewsletterView,
+    NewsletterEmailUnsubscribe,
+    NewsletterConfirmUnsubscribe,
+    NewsletterUnsubscribeDone,
+)
 
 urlpatterns = [
     path('', NewsletterView.as_view(), name='newsletter_signup'),
+    path('unsubscribe/<str:id>', NewsletterEmailUnsubscribe.as_view(), name='newsletter_unsubscribe'),
+    path(
+        'unsubscribe/<str:id>/confirm',
+        NewsletterConfirmUnsubscribe.as_view(),
+        name='newsletter_confirm',
+    ),
+    path('unsubscribe', NewsletterUnsubscribeDone.as_view(), name='newsletter_unsubscribe_done'),
 ]
