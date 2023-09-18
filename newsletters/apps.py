7b01ace8b7180bc635d90b84c2e7e84012cc3eb5
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-18 21:56:28 +0200

    add signal path to newsletter app

--- newsletters/apps.py
+++ newsletters/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NewslettersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newsletters'
+
+    def ready(self):
+        import newsletters.signals
