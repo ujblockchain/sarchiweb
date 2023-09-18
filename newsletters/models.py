34e10487de64033c3c2fe15e166d7a3a4f440f57
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-18 21:55:22 +0200

    send newsletter email model

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from shortuuid.django_fields import ShortUUIDField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+groups = (
+    ('Selected Bootcamp Group', 'Selected Bootcamp Group'),
+    ('Rejected Bootcamp Group', 'Rejected Bootcamp Group'),
+    ('Newsletter Email Group', 'Newsletter Email Group'),
+)
 
 
 class NewsletterEmail(models.Model):
@@ -14,3 +21,44 @@ class NewsletterEmail(models.Model):
     email = models.EmailField()
     date_created = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.id
+
+
+class SendNewsletterEmails(models.Model):
+    # A primary key ID of length 16 and a short alphabet.
+    id = ShortUUIDField(
+        length=16,
+        max_length=40,
+        alphabet="abcdefg1234",
+        primary_key=True,
+    )
+    subject = models.CharField(max_length=200)
+    group = models.CharField(choices=groups, max_length=100, default='Selected Bootcamp Group')
+    salutation = message = RichTextUploadingField(
+        max_length=5000,
+        help_text='to learn more about what format or the style \
+                tab content mean kindly click\
+                <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element"\
+                target="_blank" style="color:wheat;">Here</a>.',
+    )
+    message = RichTextUploadingField(
+        max_length=5000,
+        help_text='to learn more about what format or the style \
+                tab content mean kindly click\
+                <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element"\
+                target="_blank" style="color:wheat;">Here</a>.',
+    )
+    link = models.URLField()
+    link_title = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = "Send Newsletter Email"
+        verbose_name_plural = "Send Newsletter Emails"
