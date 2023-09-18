3f1d8ae73e1ba47ec089ea7f87c1df9020865974
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-18 21:55:52 +0200

    register send newsletter email model in admin

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NewsletterEmail
+from .models import NewsletterEmail, SendNewsletterEmails
 
 
 class NewsletterAdmin(admin.ModelAdmin):
@@ -33,5 +33,79 @@ class NewsletterAdmin(admin.ModelAdmin):
     fields = ['id', 'email', 'timestamp']
 
 
-#
+class SendNewsletterEmailsAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'subject',
+        'group',
+        'timestamp',
+    ]
+    list_display_links = [
+        'id',
+        'subject',
+        'group',
+        'timestamp',
+    ]
+    list_filter = ['group']
+    search_fields = [
+        'id',
+        'subject',
+        'group',
+        'link',
+        'link_title',
+        'timestamp',
+    ]
+    readonly_fields = [
+        'id',
+    ]
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fieldsets = [
+        [
+            'General Information',
+            {
+                'classes': ['wide'],
+                'fields': [
+                    'id',
+                    'subject',
+                    'group',
+                ],
+            },
+        ],
+        [
+            'Message Details',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': [
+                    'message',
+                    'salutation',
+                ],
+            },
+        ],
+        [
+            'Link Details',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': ['link', 'link_title'],
+            },
+        ],
+        [
+            'Last Updated',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': [
+                    'timestamp',
+                ],
+            },
+        ],
+    ]
+
+
+# register models
 admin.site.register(NewsletterEmail, NewsletterAdmin)
+admin.site.register(SendNewsletterEmails, SendNewsletterEmailsAdmin)
