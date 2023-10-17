ca5e60c49605dae77a3edaad9128631e2c4bdade
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-10-17 09:10:43 +0200

    register signal in program app

---
+++ program/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class ProgramConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'program'
+
+    def ready(self):
+        import program.signals
