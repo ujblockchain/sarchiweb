3864e1ea69da30f2c81c5a8b326a4c5e5dbe8834
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-10-24 07:24:56 +0200

    event url path

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, EventView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+     path('mb', EventView.as_view(), name='home'),
 ]
