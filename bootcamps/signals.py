c6c1b369727d5b85a3aa0487eeb21c385c11075e
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-02-14 22:13:12 +0200

    change model name

--- bootcamps/signals.py
+++ bootcamps/signals.py
@@ -5,14 +5,18 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.db.models.signals import pre_save, post_save
 from decouple import config
-from .models import BootcampFirst
+from .models import Bootcamp
 
 
-@receiver(post_save, sender=BootcampFirst)
+@receiver(post_save, sender=Bootcamp)
 def auto_mail_sending(sender, instance, created, **kwargs):
-    #avoid sending empty mails
-    if created or instance.application_status == 'Selected' or instance.application_status == 'Rejected':
-        
+    # avoid sending empty mails
+    if (
+        created
+        or instance.application_status == 'Selected'
+        or instance.application_status == 'Rejected'
+    ):
+
         # send email on registration complete
         sender_email = settings.DEFAULT_FROM_EMAIL
         recipient_email = [instance.email]
@@ -50,7 +54,10 @@ def auto_mail_sending(sender, instance, created, **kwargs):
                 sender_email,
                 recipient_email,
                 reply_to=[config('ADMIN_REPLY_EMAIL')],
-                headers={'X-PM-Message-Stream': 'outbound', 'Message-ID': f'{randint(1, 1000)}'},
+                headers={
+                    'X-PM-Message-Stream': 'outbound',
+                    'Message-ID': f'{randint(1, 1000)}',
+                },
             )
 
             # ensure that email format is html
@@ -62,16 +69,22 @@ def auto_mail_sending(sender, instance, created, **kwargs):
 
             # send email
             msg.send()
-        else:
+
+        elif (
+            instance.application_status == 'Selected'
+            or instance.application_status == 'Rejected'
+        ):
             # once model is saved, trigger signal
             if instance.application_status == 'Selected':
-                email_subject = 'You Have Been Selected 🥳🎉: Demystifying Blockchain Bootcamp'
-                email_message = f'Hi {instance.first_name},\n\nThank you for your interest and consequent application to the Demystifying Blockchain Bootcamp. \n\nYour interest in coding and desire to upskill aligns with our vision of building practical capacity in blockchain.\n\n We are glad to inform you that you have been selected for the Demystifying Blockchain Bootcamp. In this training, you will be taken step by step from beginning to knowing and coding in blockchain.\n\n The venue for this training is <strong>Westcliff Suite (D), Garden Court Millpark Hotel, Auckland Park,  Johannesburg, Gauteng, South Africa, with free transportation provided from APK campus Gate 2 every day of the training.</strong>. You are only to come with your computer; the rest will be provided, including tea breaks and lunch for every training day. We will also offer coding support during and after the Bootcamp.\n\nOnce again, on behalf of our entire team, a big congratulations. We are excited and ready to begin this fantastic journey of Blockchain development, step by step, with You!🤩🙌.'
+                email_subject = (
+                    'You Have Been Selected 🥳🎉: Demystifying Blockchain Bootcamp'
+                )
+                email_message = f'Hi {instance.first_name},\n\nThank you for your interest and consequent application to the Demystifying Blockchain Bootcamp. \n\nYour interest in coding and desire to upskill aligns with our vision of building practical capacity in blockchain.\n\n We are glad to inform you that you have been selected for the Demystifying Blockchain Bootcamp. In this training, you will be taken step by step from beginning to knowing and coding in blockchain.\n\n The venue for this training is <strong>School of Tourism and Hospitality (STH), APB Campus, University of Johannesburg, with free transportation provided from APK campus Gate 2 every day of the training.</strong>. You are only to come with your computer; the rest will be provided, including tea breaks and lunch for every training day. We will also offer coding support during and after the Bootcamp.\n\nOnce again, on behalf of our entire team, a big congratulations. We are excited and ready to begin this fantastic journey of Blockchain development, step by step, with You!🤩🙌.'
                 email_link = 'https://blockchain.uj.ac.za'
                 email_link_title = 'UJ Blockchain'
             elif instance.application_status == 'Rejected':
                 email_subject = 'Application Status: Demystifying Blockchain Bootcamp'
-                email_message = f'Hi {instance.first_name},\n\nThank you for your interest and consequent application to the Demystifying Blockchain Bootcamp. \n\nYour interest in coding and desire to upskill aligns with our vision of building practical capacity in blockchain.\n\n Unfortunately, you were not selected for the Bootcamp. We received large volumes of applications for the training, but we could only take a few for the Bootcamp. \n\n We understand your desire to attend the Bootcamp and your drive to upskill in Blockchain; yes, we do. There will be other Bootcamps and Hackathon coming up starting from October 2023. We will be having our Hackathon come October 2023; this Hackathon also comes with a hands-on where you will be taught step by step by our development team. There are also more Bootcamps coming up in 2024.\n\n To ensure you register in time, sign up for our newsletter. Our newsletters gives you up-to-date insight into our build stacks and projects we are currently working on. It also gives you the privilege of getting early registration links for Bootcamps, Hackathon, and other programs three days before it is made open to the general public.\n\n We are excited and look forward to seeing you in our future Bootcamps and Hackathon as we hope to begin a  fantastic journey of Blockchain development, step by step, with You!🤩🙌.'
+                email_message = f'Hi {instance.first_name},\n\nThank you for your interest and consequent application to the Demystifying Blockchain Bootcamp. \n\nYour interest in coding and desire to upskill aligns with our vision of building practical capacity in blockchain.\n\n Unfortunately, you were not selected for the Bootcamp. We received large volumes of applications for the training, but we could only take a few for the Bootcamp. \n\n We understand your desire to attend the Bootcamp and your drive to upskill in Blockchain; yes, we do. There will be other Bootcamp, Hackathon, Media Events and Project Demo coming up starting from May 2024. We will be having the next bootcamp come September 2024; this Bootcamp also comes with a hands-on where you will be taught step by step by our development team.\n\n To ensure you register in time, sign up for our newsletter. Our newsletters gives you up-to-date insight into our build stacks and projects we are currently working on. It also gives you the privilege of getting early registration links for Bootcamps, Hackathon, and other programs three days before it is made open to the general public.\n\n We are excited and look forward to seeing you in our future Bootcamps and Hackathon as we hope to begin a  fantastic journey of Blockchain development, step by step, with You!🤩🙌.'
                 email_link = 'https://blockchain.uj.ac.za/#newsletter'
                 email_link_title = 'Newsletter'
 
@@ -94,7 +107,10 @@ def auto_mail_sending(sender, instance, created, **kwargs):
                 sender_email,
                 recipient_email,
                 reply_to=[config('ADMIN_REPLY_EMAIL')],
-                headers={'X-PM-Message-Stream': 'outbound', 'Message-ID': f'{randint(1, 1000)}'},
+                headers={
+                    'X-PM-Message-Stream': 'outbound',
+                    'Message-ID': f'{randint(1, 1000)}',
+                },
             )
 
             # ensure that email format is html
