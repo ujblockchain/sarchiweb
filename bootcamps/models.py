69894dbd1fc8e030c96dae8ed047bbea3eb12f53
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-02-14 22:09:27 +0200

    add more model fields

--- bootcamps/models.py
+++ bootcamps/models.py
@@ -24,20 +24,60 @@ gender = (
     ('Male', 'Male'),
 )
 
+can_code = (
+    ('Yes I can code in HTML, CSS & Python', 'Yes I can code in HTML, CSS & Python'),
+    ('No I can not code in HTML, CSS & Python', 'No I can not code in HTML, CSS & Python'),
+)
 
-class BootcampFirst(models.Model):
+training_session = (
+    (
+        'No Coding Session (Drag and Drop Design)',
+        'No Coding Session (Drag and Drop Design)',
+    ),
+    (
+        'Coding Session (Requires basic knowledge of HTML, CSS & Python)',
+        'Coding Session (Requires basic knowledge of HTML, CSS & Python)',
+    ),
+)
+
+
+class Bootcamp(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     gender = models.CharField(max_length=50, choices=gender, default='Female')
     faculty = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     level = models.CharField(max_length=50, choices=level, default='Undergrad')
     student_number = models.BigIntegerField(default=1)
-    nationality = models.CharField(max_length=50, choices=nationality, default='South Africa')
+    nationality = models.CharField(
+        max_length=50,
+        choices=nationality,
+        default='South Africa',
+    )
     phone_number = models.CharField(max_length=30, default='')
+    session = models.CharField(
+        max_length=100,
+        choices=training_session,
+        help_text='select training session',
+    )
+    can_you_code = models.CharField(
+        max_length=50,
+        choices=can_code,
+        default='No',
+        null=True,
+        blank=True,
+        help_text='can you code in HTML, CSS & Python',
+    )
+    repo_link = models.URLField(
+        null=True,
+        blank=True,
+        help_text='code sample link',
+    )
     expectation = models.TextField(max_length=800)
-    application_status = models.CharField(null=True, blank=True, choices=applicant_selection)
+    application_status = models.CharField(
+        null=True, blank=True, choices=applicant_selection
+    )
     date_created = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(default=timezone.now)
 
@@ -46,5 +86,5 @@ class BootcampFirst(models.Model):
 
     class Meta:
         ordering = ['-date_created']
-        verbose_name = "Bootcamp Sep 2023"
-        verbose_name_plural = "Bootcamp Sep 2023"
+        verbose_name = "Bootcamp"
+        verbose_name_plural = "Bootcamps"
