a6a41af6bf341e2421fd246bf543231ed33d0551
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-16 13:03:56 +0200

    checks for registration open and close date

--- bootcamps/views.py
+++ bootcamps/views.py
@@ -1,21 +1,29 @@
-import json
+from datetime import datetime
+from django.utils import timezone
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.urls import reverse
-from django.contrib import messages
 from django.views import View
 
 from contact.forms import UserMessageForm
 from newsletters.forms import NewsletterEmailForm
 from partners.models import Partners
+from settings.models import BootcampSettings
 from .forms import BootcampForm
 from .models import Bootcamp
 
 
 class BootcampView(View):
+
     def get(self, request, *args, **kwargs):
         # set template
-        template_name = 'forms/training-form.html'
+        template_name = 'forms/bootcamp.html'
+        # init bootcamp settings
+        # get latest record
+        bootcamp_settings = BootcampSettings.objects.order_by('-date_created').all()[0]
+
+        #init current date
+        # make timezone aware
+        current_timestamp = timezone.now()
 
         # init context
         context = {
@@ -24,17 +32,20 @@ class BootcampView(View):
             'partners': Partners.objects.filter(publish=True),
             # newsletter form
             'newsletter_form': NewsletterEmailForm(),
+            # bootcamp settings
+            'registration_open': current_timestamp > bootcamp_settings.opening_date,
+            'registration_closed': current_timestamp > bootcamp_settings.closing_date
         }
 
         return render(request, template_name, context)
 
     # post request
-    def post(self, request,  *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         form = BootcampForm(request.POST)
 
         # check if request is ajax
         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        
+
             # check if form is valid
             if form.is_valid():
                 # init bound form field
@@ -54,7 +65,7 @@ class BootcampView(View):
                 if form.cleaned_data['can_you_code'] == 'Can you code in HTML, CSS & Python':
                     can_you_code = 'No I can not code in HTML, CSS & Python'
                 else:
-                   can_you_code = form.cleaned_data['can_you_code'] 
+                    can_you_code = form.cleaned_data['can_you_code']
 
                 # create model instance
                 Bootcamp.objects.create(
@@ -74,12 +85,10 @@ class BootcampView(View):
                     repo_link=repo_link,
                 )
 
-                return JsonResponse(
-                    {
-                        "message": "success",
-                        "status": "Thank You. Your Application has been Submitted Successfully.",
-                    }
-                )
+                return JsonResponse({
+                    "message": "success",
+                    "status": "Thank You. Your Application has been Submitted Successfully.",
+                })
 
             else:
                 return JsonResponse({"message": "error", "error": form.errors})
