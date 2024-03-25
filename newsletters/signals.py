130850ee93fd677fd0c4efaaeeb725ff82f81e00
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-25 06:58:03 +0200

    add signal for bootcamp coding and non coding sessions

--- newsletters/signals.py
+++ newsletters/signals.py
@@ -1,12 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from bootcamps.models import Bootcamp
-from .models import NewsletterEmail, SendNewsletterEmails
+from .models import NewsletterEmail, SendUserEmails
 
 from .tasks import send_email_task
 
 
-@receiver(post_save, sender=SendNewsletterEmails)
+@receiver(post_save, sender=SendUserEmails)
 def auto_mail_sending(sender, instance, created, **kwargs):
     # send emails once, only on create
     if created:
@@ -18,16 +18,21 @@ def auto_mail_sending(sender, instance, created, **kwargs):
         ## for those selected for bootcamp
         if group == 'Selected Bootcamp Group':
             # get the email list
-            email_list = Bootcamp.objects.filter(
-                application_status='Selected'
-            ).values_list('email', flat=True)
+            email_list = Bootcamp.objects.filter(application_status='Selected').values_list('email', flat=True)
 
         ## for those not selected for bootcamps
         elif group == 'Rejected Bootcamp Group':
             # get the email list
+            email_list = Bootcamp.objects.filter(application_status='Rejected').values_list('email', flat=True)
+        elif group == 'Bootcamp: Coding Session':
+            # get the email list
             email_list = Bootcamp.objects.filter(
-                application_status='Rejected'
-            ).values_list('email', flat=True)
+                training_session='Coding Session (Requires basic knowledge of HTML, CSS & Python)').values_list(
+                    'email', flat=True)
+        elif group == 'Bootcamp: Drag and Drop Session':
+            # get the email list
+            email_list = Bootcamp.objects.filter(
+                training_session='No Coding Session (Drag and Drop Design)').values_list('email', flat=True)
         ## for for news letter Signup
         elif group == 'Newsletter Email Group':
             # get the email list
@@ -36,10 +41,8 @@ def auto_mail_sending(sender, instance, created, **kwargs):
         # init number of  elements each list should have
         n = 15  # 15 per list
 
-        # using list comprehension to split email list into multiple lists
-        sorted_email_list = [
-            email_list[i : i + n] for i in range(0, len(email_list), n)
-        ]
+        # using list comprehension to split email list into multiple lists in a single list
+        sorted_email_list = [email_list[i:i + n] for i in range(0, len(email_list), n)]
 
         # init task
         send_email_task(
