c5b1f3ac1d6dd7797408326798f51ba717b49f3d
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-28 12:52:31 +0200

    add send user sms model

---
+++ sms/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
+from shortuuid.django_fields import ShortUUIDField
+
+groups = (
+    ('Selected Bootcamp Group', 'Selected Bootcamp Group'),
+    ('Bootcamp: Coding Session', 'Bootcamp: Coding Session'),
+    ('Bootcamp: Drag and Drop Session', 'Bootcamp: Drag and Drop Session'),
+)
+
+
+class SendUserSms(models.Model):
+    # A primary key ID of length 16 and a short alphabet.
+    id = ShortUUIDField(
+        length=16,
+        max_length=40,
+        alphabet='abcdefg1234',
+        primary_key=True,
+    )
+    title = models.CharField(max_length=200, help_text='title is not used in sms')
+    group = models.CharField(choices=groups, max_length=100, default='select bootcamp group')
+    message = models.TextField(
+        max_length=80,
+        help_text='sms message',
+    )
+    schedule_message = models.DateTimeField(default=datetime.now, help_text='specify when message will deliver')
+    date_created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = 'Send SMS To USer'
+        verbose_name_plural = 'Send SMS To USers'
