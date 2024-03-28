b2d8dd9c3e4ca23d8965b9928eb026a17726f98b
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-28 13:18:21 +0200

    email reminder utils

--- newsletters/utilities/send_email.py
+++ newsletters/utilities/send_email.py
@@ -3,7 +3,6 @@ from django.core import mail
 from django.conf import settings
 from django.template.loader import render_to_string
 from decouple import config
-from sarchi.middleware.current_request import RequestMiddleware
 
 
 # handles email sending
@@ -19,13 +18,13 @@ def send_email(salutation, subject, message, link, link_title, email_list):
 
     # use email template
     html_content = render_to_string(
-        "email/email.html",
+        'email/email.html',
         {  # pass context to email template
-            "email_subject": email_subject,
-            "email_message": email_message,
-            "email_link": email_link,
-            "email_link_title": email_link_title,
-            "email_salutation": email_salutation,
+            'email_subject': email_subject,
+            'email_message': email_message,
+            'email_link': email_link,
+            'email_link_title': email_link_title,
+            'email_salutation': email_salutation,
         },
     )
 
@@ -35,15 +34,68 @@ def send_email(salutation, subject, message, link, link_title, email_list):
         body=html_content,
         from_email=sender_email,
         bcc=recipient_email,
-        reply_to=[config("ADMIN_REPLY_EMAIL")],
+        reply_to=[config('ADMIN_REPLY_EMAIL')],
         headers={
-            "X-PM-Message-Stream": "outbound",
-            "Message-ID": f"{randint(1, 1000)}",
+            'X-PM-Message-Stream': 'outbound',
+            'Message-ID': f'{randint(1, 1000)}',
         },
     )
 
     # ensure that email format is html
-    msg.content_subtype = "html"
+    msg.content_subtype = 'html'
+
+    # send email
+    msg.send()
+
+
+# handles email sending
+def send_bootcamp_reminder_email(subject, main_message_heading, primary_message_section, image_session_heading,
+                                 display_image_1, display_image_1_text, display_image_2, display_image_2_text,
+                                 display_image_3, display_image_3_text, display_image_4, display_image_4_text,
+                                 secondary_message_section, email_list):
+    # init email details
+    sender_email = settings.DEFAULT_FROM_EMAIL
+    recipient_email = email_list
+    email_subject = subject
+
+    # use email template
+    html_content = render_to_string(
+        'email/bootcamp.html',
+        {  # pass context to email template
+            'main_message_heading': main_message_heading,
+            'primary_message_section': primary_message_section,
+            'image_session_heading': image_session_heading,
+            'display_image_1': display_image_1,
+            'display_image_1_text': display_image_1_text,
+            'display_image_2': display_image_2,
+            'display_image_2_text': display_image_2_text,
+            'display_image_3': display_image_3,
+            'display_image_3_text': display_image_3_text,
+            'display_image_4': display_image_4,
+            'display_image_4_text': display_image_4_text,
+            'secondary_message_section': secondary_message_section
+        },
+    )
+
+    # create HTML email.
+    msg = mail.EmailMessage(
+        subject=email_subject,
+        body=html_content,
+        from_email=sender_email,
+        bcc=recipient_email,
+        reply_to=[config('ADMIN_REPLY_EMAIL')],
+        headers={
+            'X-PM-Message-Stream': 'outbound',
+            'Message-ID': f'{randint(1, 1000)}',
+        },
+    )
+
+    # ensure that email format is html
+    msg.content_subtype = 'html'
+
+    # attached bootcamp flier
+    bootcamp_flyer = f'{settings.PROJECT_DIR}/static/images/Bootcamp.png'
+    msg.attach_file(bootcamp_flyer)
 
     # send email
     msg.send()
