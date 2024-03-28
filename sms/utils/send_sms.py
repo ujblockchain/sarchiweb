c390c12d78e1d93d9e3e537df732731c35692a54
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-28 12:58:37 +0200

    utils for twilio sms sending

---
+++ sms/utils/send_sms.py
@@ -0,0 +1,17 @@
+from decouple import config
+from twilio.rest import Client
+
+# init sms client
+client = Client(config('TWILIO_ACCOUNT_SID'), config('TWILIO_AUTH_TOKEN'))
+
+
+# handles sms sending
+def send_sms(phone_number, message, schedule):
+    # send message
+    client.messages.create(
+        body=f'{message}',
+        from_=config('TWILIO_PHONE_NUMBER'),
+        to=f'{phone_number}',
+        attempt=2,
+        send_at=schedule,
+    )
