f26a5d12ab728e4150b5a6801da3ac85ccbd95bb
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-28 12:48:59 +0200

    update admin

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,28 +1,12 @@
 from django.contrib import admin
-from .models import NewsletterEmail, SendUserEmails
+from .models import NewsletterEmail, SendBootcampReminderEmails, SendUserEmails
 
 
 class NewsletterAdmin(admin.ModelAdmin):
-    list_display = [
-        'id',
-        'email',
-        'timestamp',
-    ]
-    list_display_links = [
-        'id',
-        'email',
-        'timestamp',
-    ]
-    search_fields = [
-        'id',
-        'email',
-        'timestamp',
-    ]
-    readonly_fields = [
-        'id',
-        'email',
-        'timestamp',
-    ]
+    list_display = ['id', 'email', 'timestamp']
+    list_display_links = ['id', 'email', 'timestamp']
+    search_fields = ['id', 'email', 'timestamp']
+    readonly_fields = ['id', 'email', 'timestamp']
     list_per_page = 50
     show_full_result_count = True
     actions_on_top = True
@@ -34,30 +18,11 @@ class NewsletterAdmin(admin.ModelAdmin):
 
 
 class SendUserEmailsAdmin(admin.ModelAdmin):
-    list_display = [
-        'id',
-        'subject',
-        'group',
-        'timestamp',
-    ]
-    list_display_links = [
-        'id',
-        'subject',
-        'group',
-        'timestamp',
-    ]
+    list_display = ['id', 'subject', 'group', 'timestamp']
+    list_display_links = ['id', 'subject', 'group', 'timestamp']
     list_filter = ['group']
-    search_fields = [
-        'id',
-        'subject',
-        'group',
-        'link',
-        'link_title',
-        'timestamp',
-    ]
-    readonly_fields = [
-        'id',
-    ]
+    search_fields = ['id', 'subject', 'group', 'link', 'link_title', 'timestamp']
+    readonly_fields = ['id']
     list_per_page = 50
     show_full_result_count = True
     actions_on_top = True
@@ -70,21 +35,14 @@ class SendUserEmailsAdmin(admin.ModelAdmin):
             'General Information',
             {
                 'classes': ['wide'],
-                'fields': [
-                    'id',
-                    'subject',
-                    'group',
-                ],
+                'fields': ['id', 'subject', 'group'],
             },
         ],
         [
             'Message Details',
             {
                 'classes': ['collapse', 'wide'],
-                'fields': [
-                    'message',
-                    'salutation',
-                ],
+                'fields': ['message', 'salutation'],
             },
         ],
         [
@@ -98,9 +56,60 @@ class SendUserEmailsAdmin(admin.ModelAdmin):
             'Last Updated',
             {
                 'classes': ['collapse', 'wide'],
+                'fields': ['timestamp',],
+            },
+        ],
+    ]
+
+
+class ReminderEmailsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'group', 'timestamp']
+    list_display_links = ['id', 'subject', 'group', 'timestamp']
+    list_filter = ['group']
+    search_fields = [
+        'id', 'subject', 'group', 'main_message_heading', 'timestamp', 'primary_message_section',
+        'display_image_1_text', 'display_image_2_text', 'display_image_3_text', 'display_image_4_text',
+        'secondary_message_section'
+    ]
+    readonly_fields = ['id', 'timestamp']
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fieldsets = [
+        [
+            'General Information',
+            {
+                'classes': ['wide'],
+                'fields': ['id', 'subject', 'group'],
+            },
+        ],
+        [
+            'Message Details',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': ['main_message_heading', 'primary_message_section', 'secondary_message_section'],
+            },
+        ],
+        [
+            'Image Details',
+            {
+                'classes': ['collapse', 'wide'],
                 'fields': [
-                    'timestamp',
+                    'image_section_heading', 'display_image_1', 'display_image_1_text', 'display_image_2',
+                    'display_image_2_text', 'display_image_3', 'display_image_3_text', 'display_image_4',
+                    'display_image_4_text'
                 ],
+            },
+        ],
+        [
+            'Last Updated',
+            {
+                'classes': ['collapse', 'wide'],
+                'fields': ['timestamp',],
             },
         ],
     ]
@@ -109,3 +118,4 @@ class SendUserEmailsAdmin(admin.ModelAdmin):
 # register models
 admin.site.register(NewsletterEmail, NewsletterAdmin)
 admin.site.register(SendUserEmails, SendUserEmailsAdmin)
+admin.site.register(SendBootcampReminderEmails, ReminderEmailsAdmin)
