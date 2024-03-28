389cd6391b9c25e14f4e7a3639532c75895a62b8
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-28 15:03:43 +0200

    update app model

--- bootcamps/models.py
+++ bootcamps/models.py
@@ -50,17 +50,9 @@ class Bootcamp(models.Model):
     department = models.CharField(max_length=100)
     level = models.CharField(max_length=50, choices=level, default='Undergrad')
     student_number = models.BigIntegerField(default=1)
-    nationality = models.CharField(
-        max_length=50,
-        choices=nationality,
-        default='South Africa',
-    )
+    nationality = models.CharField(max_length=50, choices=nationality, default='South Africa')
     phone_number = models.CharField(max_length=30, default='')
-    session = models.CharField(
-        max_length=100,
-        choices=training_session,
-        help_text='select training session',
-    )
+    session = models.CharField(max_length=100, choices=training_session, help_text='select training session')
     can_you_code = models.CharField(
         max_length=50,
         choices=can_code,
@@ -69,11 +61,7 @@ class Bootcamp(models.Model):
         blank=True,
         help_text='can you code in HTML, CSS & Python',
     )
-    repo_link = models.URLField(
-        null=True,
-        blank=True,
-        help_text='code sample link',
-    )
+    repo_link = models.URLField(null=True, blank=True, help_text='code sample link')
     expectation = models.TextField(max_length=800)
     application_status = models.CharField(null=True, blank=True, choices=applicant_selection)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -84,5 +72,5 @@ class Bootcamp(models.Model):
 
     class Meta:
         ordering = ['-date_created']
-        verbose_name = "Bootcamp"
-        verbose_name_plural = "Bootcamps"
+        verbose_name = 'Bootcamp'
+        verbose_name_plural = 'Bootcamps'
