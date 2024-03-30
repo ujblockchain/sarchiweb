fad3bbe0b2183243d048a86b71df65c4b4784074
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-31 01:31:01 +0200

    add filter for sri file hash integrity generation

--- sarchi/jinja/env.py
+++ sarchi/jinja/env.py
@@ -1,24 +1,21 @@
 from django.urls import reverse
 from django.contrib.staticfiles.storage import staticfiles_storage
 from jinja2 import Environment
-from .filters import dateformat, dict_string
+from .filters import dateformat, dict_string, static_integrity
 
 
 def JinjaEnvironment(**options):
     env = Environment(**options)
-    env.globals.update(
-        {
-            'static': staticfiles_storage.url,
-            'url': reverse,
-            'trim_blocks': True,
-            'Istrip_blocks': True,
-        }
-    )
-    env.filters.update(
-        {
-            'dateformat': dateformat,
-            'dictstring': dict_string,
-        }
-    )
+    env.globals.update({
+        'url': reverse,
+        'trim_blocks': True,
+        'Istrip_blocks': True,
+        'static': staticfiles_storage.url,
+    })
+    env.filters.update({
+        'dateformat': dateformat,
+        'dictstring': dict_string,
+        'static_integrity': static_integrity,
+    })
 
     return env
