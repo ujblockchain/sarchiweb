6506f4d66e66b85b4386c78d0a4593b50eb411d0
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-31 16:24:41 +0200

    remove handlers

--- sarchi/urls.py
+++ sarchi/urls.py
@@ -4,13 +4,6 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from .utils.views import permission_denied, page_not_found, server_error
 
-# Overrides the default 403 handler
-handler403 = 'sarchi.utils.views.permission_denied'
-# Overrides the default 404 handler
-handler404 = 'sarchi.utils.views.page_not_found'
-# Overrides the default 500 handler
-handler500 = 'sarchi.utils.views.server_error'
-
 urlpatterns = [
     # admin path
     path(f'{settings.ADMIN_PATH}/', admin.site.urls),
