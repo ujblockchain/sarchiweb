5ec8b34b149e5b1e22b9f3efc6207f1ea7062a0d
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-31 16:17:20 +0200

    register handlers

--- sarchi/urls.py
+++ sarchi/urls.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from .utils.views import permission_denied, page_not_found, server_error
+
+# Overrides the default 403 handler
+handler403 = 'sarchi.utils.views.permission_denied'
+# Overrides the default 404 handler
+handler404 = 'sarchi.utils.views.page_not_found'
+# Overrides the default 500 handler
+handler500 = 'sarchi.utils.views.server_error'
 
 urlpatterns = [
     # admin path
