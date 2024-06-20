a426e3049c4ea6f536fae284dec542bc9d5f783d
edmundrotimi <edmundrotimi@gmail.com>
2024-06-20 07:01:36 +0200

    add masterclass model settings

--- settings/models.py
+++ settings/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 
-class BootcampSettings(models.Model):
-    bootcamp_title = models.CharField(max_length=200)
-    opening_date = models.DateTimeField(default=timezone.now, help_text='Time registration starts')
+class TrainingSettingsBase(models.Model):
+    opening_date = models.DateTimeField(default=timezone.now,
+                                        help_text='Time registration starts')
     closing_date = models.DateTimeField(help_text='Time registration closes')
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.bootcamp_title
+
+    class Meta:
+        abstract = True
+
+
+class BootcampSettings(TrainingSettingsBase):
+    bootcamp_title = models.CharField(max_length=200)
     last_update = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -16,3 +26,16 @@ class BootcampSettings(models.Model):
         ordering = ['-date_created']
         verbose_name = 'Bootcamp Settings'
         verbose_name_plural = 'Bootcamp Settings'
+
+
+class MasterclassSettings(TrainingSettingsBase):
+    masterclass_title = models.CharField(max_length=200)
+    last_update = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.masterclass_title
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name = 'Masterclass Settings'
+        verbose_name_plural = 'Masterclass Settings'
