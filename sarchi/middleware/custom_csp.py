1cb4da2b5a588946ac2ea714f62699fa775858c8
edmundrotimi <edmundrotimi@gmail.com>
2024-06-20 03:56:10 +0200

    update custom CSP

--- sarchi/middleware/custom_csp.py
+++ sarchi/middleware/custom_csp.py
@@ -8,15 +8,15 @@ class CustomCSPMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        print('Content-Security-Policy' in response)
-        
 
         # Check if the response corresponds to a 404 error page
-        if isinstance(response, HttpResponseServerError) or isinstance(response, HttpResponseNotFound) or isinstance(
-                response, HttpResponseBadRequest) or isinstance(response, HttpResponseForbidden):
+        if isinstance(response, HttpResponseServerError) or isinstance(
+                response, HttpResponseNotFound) or isinstance(
+                    response, HttpResponseBadRequest) or isinstance(
+                        response, HttpResponseForbidden):
 
             # Apply the relaxed CSP policy for error pages
-           response['Content-Security-Policy'] = "default-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' https://www.google.com https://www.googletagmanager.com https://www.gstatic.com; font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com https://fonts.googleapis.com/ 'unsafe-inline';"
-
+            response[
+                'Content-Security-Policy'] = "default-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' https://www.google.com https://www.googletagmanager.com https://www.gstatic.com; font-src 'self' https://fonts.gstatic.com https://use.fontawesome.com https://fonts.googleapis.com/ 'unsafe-inline';"
 
         return response
