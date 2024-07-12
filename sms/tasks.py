2705fac9be272db025d9ff0260b07af88efe45eb
edmundrotimi <edmundrotimi@gmail.com>
2024-07-12 12:40:09 +0200

    update tasks

--- sms/tasks.py
+++ sms/tasks.py
@@ -1,15 +1,9 @@
-from django_huey import task, on_startup, signal
-from huey_monitor.tasks import startup_handler, store_signals
+from huey.contrib.djhuey import task
 from .utils.send_sms import send_sms
-
-# huey monitor setup for send emails queue.
-# this will enable us to track task in the admin
-signal(queue='send_sms')(store_signals)
-on_startup(queue='send_sms')(startup_handler)
 
 
 # set task
-@task(queue='send_sms')
+@task()
 def send_sms_task(sms_list, message):
     # send sms
     # loop number list
