be8325787eca414e8bc17708f581cdbf6adda9e5
edmundrotimi <edmundrotimi@gmail.com>
2024-09-21 20:20:33 +0200

    update signals

--- program/signals.py
+++ program/signals.py
@@ -29,24 +29,16 @@ def auto_mail_sending(sender, instance, created, **kwargs):
 
         if created:
             # send email on registration complete
-            email_subject = 'Mainstreaming Blockchain (MB)'
+            email_subject = 'UJ Blockchain Demo Day'
             email_message = f'Hi {instance.first_name},\n\n\
-                Thank you for your interest and for submitting your application to attend the Mainstreaming Blockchain (MB): \
-                A Thought Leadership Media Event. Mainstreaming Blockchain is set to take place on October 24, 2023. Organized by \
-                the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event will bring together blockchain \
-                enthusiasts, industry experts, researchers, and thought leaders. Its primary focus is to delve into the immense potential \
-                of blockchain technology in South and Sub-Saharan Africa. \n\n\
-                This event promises to be impactful as it explores the intersection of Blockchain and the Fourth Industrial Revolution. \
-                Blockchain technology is more than just another tech innovation; it lies at the core of the Fourth Industrial Revolution (4IR). \
-                It possesses the transformative power to reshape industries such as finance, healthcare, and logistics. \n\n\
-                Come gain an international perspective that transcends national borders. We have extended invitations to speakers and guests from \
-                various countries, including Kenya and Switzerland. \n\n\
-                Kindly note that your application has been received and is slated for review by our development team within 24 hours. \
-                While you wait, kindly connect with us via our website; you can also access our open-source repository \
-                (<a href="http://github.com/ujblockchain" target="_blank" style="color: #ff6522 !important;">github.com/ujblockchain</a>) \
-                for code guides and tips.\n\n\
-                Once again, thank you for applying for the Mainstreaming Blockchain Event. We are excited and cannot wait to see the many frontiers \
-                this will open for you in your Blockchain Development Journey 🤩🙌.\
+                Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
+                Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
+                in Blockchain, AI and Hardwares. \n\n\
+                From Drone Design, Computer Vision Projects, and 3d world reconstruction to decentralized projects used by thousands of users, this event promises \
+                to be impactful as we showcase our innovations and partnerships industries since the year began. \n\n\
+                Kindly note that your application has been received and is slated for review by our development team within 72 hours. \n\n\
+                Once again, thank you for applying for the Mainstreaming Blockchain Event. We are excited and cannot wait to show you some of the amazing \
+                innovations we have been working on 🤩🙌.\
             '
             email_link = 'https://blockchain.uj.ac.za'
             email_link_title = 'UJ Blockchain'
@@ -85,45 +77,36 @@ def auto_mail_sending(sender, instance, created, **kwargs):
         else:
             # once model is saved, trigger signal
             if instance.application_status == 'Selected':
-                email_subject = 'You Have Been Selected 🥳🎉: Mainstreaming Blockchain (MB)'
-                email_message = f'Hi {instance.first_name},\n\n\
-                    Thank you for your interest and selection to attend the Mainstreaming Blockchain (MB): \
-                    A Thought Leadership Media Event. Mainstreaming Blockchain is set to take place on October 24, 2023. Organized by \
-                    the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event will bring together blockchain \
-                    enthusiasts, industry experts, researchers, and thought leaders. Its primary focus is to delve into the immense potential \
-                    of blockchain technology in South and Sub-Saharan Africa. \n\n\
-                    This event promises to be impactful as it explores the intersection of Blockchain and the Fourth Industrial Revolution. \
-                    Blockchain technology is more than just another tech innovation; it lies at the core of the Fourth Industrial Revolution (4IR). \
-                    It possesses the transformative power to reshape industries such as finance, healthcare, and logistics. \n\n\
-                    Come gain an international perspective that transcends national borders. We have extended invitations to speakers and guests from \
-                    various countries, including Kenya and Switzerland. \n\n\
-                    Kindly note that you have been selected to attend the Mainstreaming Blockchain (MB) event. The event takes place at \
-                    <strong>STH Auckland Park Bunting Road Campus. Time is 8:30am </strong>. While you wait, kindly connect with us via our website; \
-                    you can also access our open-source repository (<a href="http://github.com/ujblockchain" target="_blank" style="color: #ff6522 !important;">github.com/ujblockchain</a>) \
-                    for code guides and tips.\n\n\
-                    Once again, thank you for applying for the Mainstreaming Blockchain (MB) Event. We are excited and cannot wait to see the many frontiers \
-                    this will open for you in your Blockchain Development Journey 🤩🙌.\
+                email_subject = 'You Have Been Selected 🥳🎉: UJ Blockchain Demo Day'
+                email_message = f'Hi {instance.first_name}, \n\n\
+                    Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
+                    Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
+                    in Blockchain, AI and Hardwares. \n\n\
+                    From Drone Design, Computer Vision Projects, and 3d world reconstruction to decentralized projects used by thousands of users, this event promises \
+                    to be impactful as we showcase our innovations and partnerships industries since the year began. \n\n\
+                    Kindly note that you have been selected to attend the Demo Day event. The event comes up by <strong style="color: #ff6522 !important;">9am</strong> \
+                    on the <strong style="color: #ff6522 !important;">3rd of October 2024, at the Johannesburg Business School Auditorium, JBS Park, 69 Kingsway Avenue, \
+                    Auckland Park, Johannesburg, 2092, South Africa.</strong> \n\n\
+                    Once again, thank you for applying for the Mainstreaming Blockchain Event. We are excited and cannot wait to show you some of the amazing \
+                    innovations we have been working on 🤩🙌.\
                 '
 
                 email_link = 'https://blockchain.uj.ac.za'
                 email_link_title = 'UJ Blockchain'
 
             elif instance.application_status == 'Rejected':
-                email_subject = 'Application Status: Mainstreaming Blockchain (MB)'
-                email_message = f'Hi {instance.first_name},\n\n\
-                    Thank you for your interest and selection to attend the Mainstreaming Blockchain (MB): \
-                    A Thought Leadership Media Event. Mainstreaming Blockchain is set to take place on October 24, 2023. Organized by \
-                    the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event will bring together blockchain \
-                    enthusiasts, industry experts, researchers, and thought leaders. Its primary focus is to delve into the immense potential \
-                    of blockchain technology in South and Sub-Saharan Africa. \n\n\
-                    Unfortunately, you were not selected for the Mainstreaming Blockchain (MB) event. We received large volumes of applications for \
-                    the event, but we could only take a few. \n\n\
-                    We understand your desire to attend this event and your drive to gain a better perspective in Blockchain; yes, we do. There will be \
-                    other Events coming up; to ensure you register in time, sign up for our newsletter. Our newsletters gives you up-to-date \
-                    insight into our build stacks and projects we are currently working on. It also gives you the privilege of getting early \
-                    registration links for Bootcamps, Hackathon, and other programs like this three days before it is made open to the general public.\n\n\
-                    Once again, thank you for applying for the Mainstreaming Blockchain (MB) Event. We are excited and cannot wait to see the many frontiers \
-                    upcoming events will open for you in your Blockchain Development Journey 🤩🙌.\
+                email_subject = 'Application Status: UJ Blockchain Demo Day'
+                email_message = f'Hi {instance.first_name}, \n\n\
+                    Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
+                    Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
+                    in Blockchain, AI and Hardwares. \n\n\
+                    Unfortunately, you were not selected for the Demo Day event. We received large volumes of applications for the event, but we could only take a few. \
+                    We understand your desire to attend this event and your drive to gain a better perspective on Blockchain and 4IR; yes, we do. There will be other \
+                    Events coming up; to ensure you register in time, sign up for our newsletter. Our newsletters give you up-to-date insight into our build stacks and \
+                    projects we are currently working on. It also gives you the privilege of getting early registration links for Bootcamps, Hackathon, and other programs \
+                    like this three days before it is made open to the public. \n\n\
+                    Once again, thank you for applying for the UJ Blockchain Demo Day Event. We are excited and cannot wait to see the many frontiers upcoming \
+                    events will open for you in your Blockchain Development Journey 🤩🙌. \
                 '
 
                 email_link = 'https://blockchain.uj.ac.za/#newsletter'
