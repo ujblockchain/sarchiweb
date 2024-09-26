769b1fe2740b3f7add2cdd45ea797c7a3b712323
edmundrotimi <edmundrotimi@gmail.com>
2024-09-26 06:46:20 +0200

    update signals

--- program/signals.py
+++ program/signals.py
@@ -31,13 +31,17 @@ def auto_mail_sending(sender, instance, created, **kwargs):
             # send email on registration complete
             email_subject = 'UJ Blockchain Demo Day'
             email_message = f'Hi {instance.first_name},\n\n\
-                Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
-                Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
-                in Blockchain, AI and Hardwares. \n\n\
-                From Drone Design, Computer Vision Projects, and 3d world reconstruction to decentralized projects used by thousands of users, this event promises \
-                to be impactful as we showcase our innovations and partnerships industries since the year began. \n\n\
-                Kindly note that your application has been received and is slated for review by our development team within 72 hours. \n\n\
-                Once again, thank you for applying for the Mainstreaming Blockchain Event. We are excited and cannot wait to show you some of the amazing \
+            
+                Thank you for your interest in attending the UJ Blockchain Demo Day Event, set to take place on October 3rd, 2024. Organized by the SA \
+                Swiss Chair in Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise in Blockchain, \
+                AI and hardware. \
+                
+                Projects ranging from drone designs and computer vision applications to 3D world reconstructions and decentralized solutions used by thousands \
+                will be displayed as we showcase our innovations and partnerships across industries since the year began. \n\n\
+                
+                Kindly note that your application has been received and is slated for review by our Development Team within 72 hours. \n\n\
+                
+                Once again, thank you for applying for the UJ Blockchain Demo Day Event. We are excited and cannot wait to show you some amazing \
                 innovations we have been working on 🤩🙌.\
             '
             email_link = 'https://blockchain.uj.ac.za'
@@ -69,7 +73,7 @@ def auto_mail_sending(sender, instance, created, **kwargs):
             msg.content_subtype = 'html'
 
             # attached file
-            program_flyer = f'{settings.PROJECT_DIR}/static/images/flyer.jpg'
+            program_flyer = f'{settings.PROJECT_DIR}/static/images/flyer.png'
             msg.attach_file(program_flyer)
 
             # send email
@@ -79,15 +83,19 @@ def auto_mail_sending(sender, instance, created, **kwargs):
             if instance.application_status == 'Selected':
                 email_subject = 'You Have Been Selected 🥳🎉: UJ Blockchain Demo Day'
                 email_message = f'Hi {instance.first_name}, \n\n\
-                    Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
-                    Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
-                    in Blockchain, AI and Hardwares. \n\n\
-                    From Drone Design, Computer Vision Projects, and 3d world reconstruction to decentralized projects used by thousands of users, this event promises \
-                    to be impactful as we showcase our innovations and partnerships industries since the year began. \n\n\
-                    Kindly note that you have been selected to attend the Demo Day event. The event comes up by <strong style="color: #ff6522 !important;">9am</strong> \
-                    on the <strong style="color: #ff6522 !important;">3rd of October 2024, at the Johannesburg Business School Auditorium, JBS Park, 69 Kingsway Avenue, \
-                    Auckland Park, Johannesburg, 2092, South Africa.</strong> \n\n\
-                    Once again, thank you for applying for the Mainstreaming Blockchain Event. We are excited and cannot wait to show you some of the amazing \
+        
+                    Thank you for your interest in attending the UJ Blockchain Demo Day Event, set to take place on October 3rd, 2024. Organized by the SA \
+                    Swiss Chair in Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise in Blockchain, \
+                    AI and hardware. \
+                    
+                    Projects ranging from drone designs and computer vision applications to 3D world reconstructions and decentralized solutions used by thousands \
+                    will be displayed as we showcase our innovations and partnerships across industries since the year began. \n\n\
+                    
+                    Kindly note that you have been selected to attend the Demo Day event. The event will be held at \
+                    <strong style="color: #ff6522 !important;">9 am</strong> on the \
+                    <strong style="color: #ff6522 !important;">3rd of October 2024, at the Johannesburg Business School auditorium GLV1 behind the reception</strong>. \n\n\
+ 
+                    Once again, thank you for applying for the UJ Blockchain Demo Day Event. We are excited and cannot wait to show you some amazing \
                     innovations we have been working on 🤩🙌.\
                 '
 
@@ -97,16 +105,17 @@ def auto_mail_sending(sender, instance, created, **kwargs):
             elif instance.application_status == 'Rejected':
                 email_subject = 'Application Status: UJ Blockchain Demo Day'
                 email_message = f'Hi {instance.first_name}, \n\n\
-                    Thank you for your interest and for submitting your application to attend the UJ Blockchain Demo Day, set to take place on October 3rd, 2024. \
-                    Organized by the SA/Swiss Chair on Blockchain Technology at the University of Johannesburg, this event showcases our depth of research and expertise \
-                    in Blockchain, AI and Hardwares. \n\n\
-                    Unfortunately, you were not selected for the Demo Day event. We received large volumes of applications for the event, but we could only take a few. \
-                    We understand your desire to attend this event and your drive to gain a better perspective on Blockchain and 4IR; yes, we do. There will be other \
-                    Events coming up; to ensure you register in time, sign up for our newsletter. Our newsletters give you up-to-date insight into our build stacks and \
-                    projects we are currently working on. It also gives you the privilege of getting early registration links for Bootcamps, Hackathon, and other programs \
-                    like this three days before it is made open to the public. \n\n\
+                    Thank you for your interest in attending the UJ Blockchain Demo Day. Unfortunately, you were not selected for the Demo Day event. \
+                    We received large applications for the event but could only take a few\n\n\
+                    
+                    We understand your desire to attend this event and your drive to gain a better perspective on Blockchain and 4IR; yes, \
+                    we do. Other events will be coming up; sign up for our newsletter to ensure you register on time. \n\n\
+                    
+                    Our newsletters give you up-to-date insight into our build stacks and projects we are currently working on. It also gives you the privilege \
+                    of getting early registration links for Bootcamps, Hackathon, and other programs like this three days before it is made open to the public. \n\n\
+                     
                     Once again, thank you for applying for the UJ Blockchain Demo Day Event. We are excited and cannot wait to see the many frontiers upcoming \
-                    events will open for you in your Blockchain Development Journey 🤩🙌. \
+                    events will open for you in your Blockchain Development Journey 🤩🙌. 
                 '
 
                 email_link = 'https://blockchain.uj.ac.za/#newsletter'
@@ -139,7 +148,7 @@ def auto_mail_sending(sender, instance, created, **kwargs):
 
             # attached file
             if instance.application_status == 'Selected':
-                program_flyer = f'{settings.PROJECT_DIR}/static/images/flyer.jpg'
+                program_flyer = f'{settings.PROJECT_DIR}/static/images/flyer.png'
                 msg.attach_file(program_flyer)
 
             # send email
