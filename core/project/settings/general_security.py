1361c91fe68e1744bcf5339388ed6a468864bd57
edmundrotimi <edmundrotimi@gmail.com>
2025-08-07 16:03:52 +0200

    update csp

--- core/project/settings/general_security.py
+++ core/project/settings/general_security.py
@@ -29,22 +29,38 @@ RECAPTCHA_REQUIRED_SCORE = ENV.config('RECAPTCHA_REQUIRED_SCORE', cast=float)
 # CSP default settings
 CSP_DEFAULT_SRC = ("'none'",)
 CSP_SCRIPT_SRC = (
-    "'self'", 'https://google.com', 'https://www.googletagmanager.com', 'https://gstatic.com',
-    'https://ajax.googleapis.com', 'https://www.google.com', 'https://www.gstatic.com', "'unsafe-inline'"
+    "'self'",
+    'https://google.com',
+    'https://www.googletagmanager.com',
+    'https://gstatic.com',
+    'https://ajax.googleapis.com',
+    'https://www.google.com',
+    'https://www.gstatic.com',
+    "'unsafe-inline'",
 )
-CSP_FONT_SRC = ("'self'", 'https://fonts.gstatic.com', 'https://use.fontawesome.com')
+CSP_FONT_SRC = (
+    "'self'",
+    'https://fonts.gstatic.com',
+    'https://use.fontawesome.com',
+    'data:',
+)
 CSP_STYLE_SRC = (
-    "'self'", 'https://google.com', 'https://www.googletagmanager.com', 'https://gstatic.com',
-    'https://fonts.googleapis.com', 'https://use.fontawesome.com', "'unsafe-inline'"
+    "'self'",
+    'https://google.com',
+    'https://www.googletagmanager.com',
+    'https://gstatic.com',
+    'https://fonts.googleapis.com',
+    'https://use.fontawesome.com',
+    "'unsafe-inline'",
 )
 CSP_CONNECT_SRC = ("'self'", 'https://www.google-analytics.com')
 CSP_MANIFEST_SRC = ("'self'",)
 CSP_FRAME_SRC = ("'self'", 'https://www.google.com')
-CSP_IMG_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'", 'data:')
 CSP_MEDIA_SRC = ("'self'",)
 CSP_FORM_ACTION = ("'self'",)
-CSP_BASE_URI = ("'self'")
-CSP_FRAME_ANCESTORS = ("'none'")
+CSP_BASE_URI = "'self'"
+CSP_FRAME_ANCESTORS = "'none'"
 CSP_EXCLUDE_URL_PREFIXES = f'/{ADMIN_PATH}/'
 CSP_REPORT_URI = ENV.config('SENTRY_REPORT_URL')
 
