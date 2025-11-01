a1e2f3c72ab785f5bf2433ff8034aaf3b4a3c3c1
edmundrotimi <edmundrotimi@gmail.com>
2025-11-01 13:17:59 +0200

    update program event

--- core/program/signals.py
+++ core/program/signals.py
@@ -33,21 +33,22 @@ def auto_mail_sending(sender, instance, created, **kwargs):
 
         if created:
             # send email on registration complete
-            email_subject = 'UJ Blockchain Demo Day'
+            email_subject = (
+                'BACISS (Blockchain · AI · Cloud · Innovation · Startup School)'
+            )
             email_message = f'Hi {instance.first_name},\n\n\
-                Thank you for your interest in attending the UJ Blockchain Demo Day Event, set to take \
-                place on August 15th, 2025. Organized by the South Africa - \
-                Switzerland Chair in Blockchain Technology at the University of Johannesburg, this event \
-                showcases our depth of research and expertise in Blockchain, \
-                AI and Hardware. Projects ranging from drone designs, computer \
-                vision applications to AI and decentralized \
-                solutions will be displayed as we showcase our \
-                innovations since the year began. \n\n\
+                Thank you for your interest in attending BACISS (Blockchain · AI · Cloud · Innovation · Startup School), \
+                an immersive, hands-on innovation program designed to empower students, researchers, and entrepreneurs \
+                to build, innovate, and scale technology-driven ventures. The event is set to take \
+                place on 17th-21st November 2025. Organized by the South Africa - \
+                Switzerland Chair in Blockchain Technology at the University of Johannesburg in \
+                collaboration with industry partners, this event \
+                aims build, innovate, and scale technology-driven ventures. \n\n\
                 Kindly note that your application has been received and is slated for review \
                 by our Development Team. \n\n\
-                Once again, thank you for applying for the UJ Blockchain Demo Day Event. \
-                We are excited and cannot wait to show you some amazing \
-                innovations we have been working on 🤩🙌.\
+                Once again, thank you for applying for the BACISS Event. \
+                We are excited and cannot wait to see the amazing \
+                innovations you will build 🤩🙌.\
             '
 
             email_link = 'https://blockchain.uj.ac.za'
@@ -93,46 +94,46 @@ def auto_mail_sending(sender, instance, created, **kwargs):
         else:
             # once model is saved, trigger signal
             if instance.application_status == 'Selected':
-                email_subject = 'You Have Been Selected 🥳🎉: UJ Blockchain Demo Day'
+                email_subject = 'You Have Been Selected 🥳🎉: BACISS (Blockchain · AI · Cloud · Innovation · Startup School)'
                 email_message = f'Hi {instance.first_name}, \n\n\
-                    Thank you for your interest in attending the UJ Blockchain Demo Day Event, \
-                    set to take place on August 15th, 2025. Organized by the South Africa - \
-                    Switzerland Chair in Blockchain Technology at the University of Johannesburg, \
-                    this event showcases our depth of research and expertise in Blockchain, \
-                    AI and hardware. Projects ranging from drone designs and computer vision \
-                    applications to AI and decentralized solutions \
-                    will be displayed as we showcase our innovations \
-                    since the year began. \n\n\
-                    Kindly note that you have been selected to attend the Demo Day \
-                    event. The event will be held at \
-                    <strong style="color: #ff6522 !important;">9 am</strong> on the \
-                    <strong style="color: #ff6522 !important;">15th of August 2025, at the \
-                    Johannesburg Business School auditorium GLV1 behind the reception</strong>. \n\n\
-                    Once again, thank you for applying for the UJ Blockchain Demo Day Event. \
-                    We are excited and cannot wait to show you some amazing \
-                    innovations we have been working on 🤩🙌.\
+                    Thank you for your interest in attending BACISS (Blockchain · AI · Cloud · Innovation · Startup School), \
+                    an immersive, hands-on innovation program designed to empower students, researchers, and entrepreneurs \
+                    to build, innovate, and scale technology-driven ventures. The event is set to take \
+                    place on 17th-21st November 2025. Organized by the South Africa - \
+                    Switzerland Chair in Blockchain Technology at the University of Johannesburg in \
+                    collaboration with industry partners,  this event \
+                    aims build, innovate, and scale technology-driven ventures. \n\n\
+                    Kindly note that you have been selected to attend this event \
+                    The event will be held at \
+                    <strong style="color: #ff6522 !important;">9 am daily</strong> on the \
+                    <strong style="color: #ff6522 !important;">17th-21st November 2025, at \
+                    B2 Lab 219, Department of Electrical Electronics, Engineering Building, \
+                    Kingsway Campus (APK Campus), University of Johannesburg. \n\n\
+                    Once again, thank you for applying for the BACISS Event. \
+                    We are excited and cannot wait to see the amazing \
+                    innovations you will build 🤩🙌.\
                 '
 
                 email_link = 'https://blockchain.uj.ac.za'
                 email_link_title = 'UJ Blockchain'
 
             elif instance.application_status == 'Rejected':
-                email_subject = 'Application Status: UJ Blockchain Demo Day'
+                email_subject = 'Application Status: BACISS (Blockchain · AI · Cloud · Innovation · Startup School)'
                 email_message = f'Hi {instance.first_name}, \n\n\
-                    Thank you for your interest in attending the UJ Blockchain Demo Day. \
-                    Unfortunately, you were not selected for the Demo Day event. \
+                    Thank you for your interest in attending the BACISS (Blockchain · AI · Cloud · Innovation · Startup School) event. \
+                    Unfortunately, you were not selected for the event. \
                     We received large applications for the event but could only take a few\n\n\
                     We understand your desire to attend this event and your drive to gain a \
-                    better perspective on Blockchain and 4IR; yes, \
-                    we do. Other events will be coming up; sign up for our newsletter to ensure \
+                    better perspective on building startup solutions; yes, \
+                    we do. Other events like this will be coming up; sign up for our newsletter to ensure \
                     you register on time. \n\n\
                     Our newsletters give you up-to-date insight into our build stacks and projects \
                     we are currently working on. It also gives you the privilege \
                     of getting early registration links for Bootcamps, Hackathon, and other programs \
                     like this three days before it is made open to the public. \n\n\
-                    Once again, thank you for applying for the UJ Blockchain Demo Day Event. \
+                    Once again, thank you for applying for the BACISS (Blockchain · AI · Cloud · Innovation · Startup School) Event. \
                     We are excited and cannot wait to see the many frontiers upcoming \
-                    events will open for you in your Blockchain Development Journey 🤩🙌.\
+                    events will open for you in your startup Journey 🤩🙌.\
                 '
 
                 email_link = 'https://blockchain.uj.ac.za/#newsletter'
