cd659b4b194798f06db75d4163c14122f7799b36
edmundrotimi <edmundrotimi@gmail.com>
2026-02-06 14:52:36 +0200

    update pages app

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.base import TemplateView
+from contact.views import ContactFormMixin
+
 
 class AccountLockout(TemplateView):
-    template_name = 'account/lockout.html'
+    template_name = 'pages/lockedout.html'
 
-    
-class HomeView(TemplateView):
+
+class HomeView(ContactFormMixin, TemplateView):
     template_name = 'pages/index.html'
