15851536aaa02607b675fee040e45b966070ee87
edmundrotimi <edmundrotimi@gmail.com>
2026-02-06 15:25:24 +0200

    update icon for daisy

--- contact/apps.py
+++ contact/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
+    icon = 'fas fa-envelope'
+    hide = False
