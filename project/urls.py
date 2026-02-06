49924dfbf6c9771306f7bc6d06380e9079cf2688
edmundrotimi <edmundrotimi@gmail.com>
2026-02-06 14:59:51 +0200

    update with custom handlers and admin otp

--- project/urls.py
+++ project/urls.py
@@ -2,11 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.admin.models import LogEntry
+from django.views.generic import RedirectView
+from project.settings.utils.otp import CustomAdminLoginView
+from project.settings import env
+
+ADMIN_PATH = env.get('ADMIN_PATH')
+
 
 urlpatterns = [
-    path(f'{settings.ADMIN_PATH}/', admin.site.urls),
-    path('', include('core.pages.urls')),
-    path('contact/', include('core.contact.urls')),
+    path(f'{ADMIN_PATH}/login/', CustomAdminLoginView.as_view(), name='admin_login'),
+    path(f'{ADMIN_PATH}/', admin.site.urls),
+    path('', include('pages.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon/favicon.ico')),
 ]
 
 # add admin path to urlpatterns
@@ -16,3 +24,18 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# inherit from the existing admin site class
+class OTPAdminSite(admin.site.__class__):
+    def get_log_entries(self, request):
+        return LogEntry.objects.all().order_by('-action_time')
+
+
+# enforce 2fa.
+admin.site.__class__ = OTPAdminSite
+# overrides the default 400, 404, 403 and 500 handler
+handler400 = 'project.settings.utils.views.bad_request'
+handler403 = 'project.settings.utils.views.permission_denied'
+handler404 = 'project.settings.utils.views.page_not_found'
+handler500 = 'project.settings.utils.views.server_error'
