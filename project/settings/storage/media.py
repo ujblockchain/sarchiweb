bd3a2a17d3e713a12742c96701e6225160ccd7c1
edmundrotimi <edmundrotimi@gmail.com>
2026-02-19 10:32:32 +0200

    custom s3 storage backend

---
+++ project/settings/storage/media.py
@@ -0,0 +1,9 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
