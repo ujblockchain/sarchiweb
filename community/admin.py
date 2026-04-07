e08e231714d9d8eeb9de13065a4ed7e614daafd2
edmundrotimi <edmundrotimi@gmail.com>
2026-04-07 09:36:04 +0200

    add export chunk size

--- community/admin.py
+++ community/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from import_export import resources
 from .models import CommunityRegistration
 from import_export.admin import ImportExportActionModelAdmin
 
 
+class CommunityResource(resources.ModelResource):
+    class Meta:
+        model = CommunityRegistration
+        chunk_size = 2000
+
+    def get_queryset(self):
+        return super().get_queryset().select_related()
+
+    def filter_export(self, queryset, **kwargs):
+        return queryset
+
+
 @admin.register(CommunityRegistration)
 class CummunityApplicationAdmin(ImportExportActionModelAdmin):
+    resource_classes = [CommunityResource]
     list_display = ['first_name', 'last_name', 'faculty', 'department', 'created_at']
     list_display_links = ['first_name', 'last_name', 'faculty', 'department']
     date_hierarchy = 'created_at'
@@ -13,7 +27,6 @@ class CummunityApplicationAdmin(ImportExportActionModelAdmin):
     readonly_fields = ['id', 'created_at']
     save_as = True
     save_as_continue = True
-    show_full_result_count = True
     save_on_top = True
     search_fields = ['first_name', 'last_name', 'faculty', 'department']
     fields = [
--- facilitator/admin.py
+++ facilitator/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from import_export import resources
 from .models import StudentApplication
 from import_export.admin import ImportExportActionModelAdmin
 
 
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = StudentApplication
+        chunk_size = 2000
+
+    def get_queryset(self):
+        return super().get_queryset().select_related()
+
+    def filter_export(self, queryset, **kwargs):
+        return queryset
+
 
 @admin.register(StudentApplication)
 class StudentApplicationAdmin(ImportExportActionModelAdmin):
+    resource_classes = [StudentResource]
     list_display = ['first_name', 'last_name', 'faculty', 'department', 'created_at']
     list_display_links = ['first_name', 'last_name', 'faculty', 'department']
     date_hierarchy = 'created_at'
@@ -13,7 +26,6 @@ class StudentApplicationAdmin(ImportExportActionModelAdmin):
     actions_on_top = True
     readonly_fields = ['id', 'created_at']
     save_as = True
-    show_full_result_count = True
     save_as_continue = True
     search_fields = ['first_name', 'last_name', 'faculty', 'department']
     fields = [
