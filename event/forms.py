fbdd371f60aeb25801b57452318bbae544bc3863
edmundrotimi <edmundrotimi@gmail.com>
2026-04-13 15:15:03 +0200

    fews form

--- event/forms.py
+++ event/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import EventRegistration
+from .models import EventRegistration, FewsRegistration
 
 
 class EventRegistrationForm(forms.ModelForm):
@@ -24,6 +24,43 @@ class EventRegistrationForm(forms.ModelForm):
         email = self.cleaned_data.get('email').lower()
 
         if EventRegistration.objects.filter(email=email).exists():
+            raise forms.ValidationError("email already signed up.")
+
+        return email
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        cleaned_phone = phone.replace(" ", "").replace("+", "")
+
+        if not cleaned_phone.isdigit():
+            raise forms.ValidationError("Phone number must contain only numbers.")
+
+        if len(cleaned_phone) < 9:
+            raise forms.ValidationError("Phone number is too short.")
+
+        return phone
+
+
+class FewsRegistrationForm(forms.ModelForm):
+    media_consent = forms.BooleanField(
+        required=True, error_messages={'required': 'You must accept the disclaimer.'}
+    )
+
+    class Meta:
+        model = FewsRegistration
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'organization',
+            'attendance_type',
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email').lower()
+
+        if FewsRegistration.objects.filter(email=email).exists():
             raise forms.ValidationError("email already signed up.")
 
         return email
