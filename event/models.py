ab7198630f42c10105825749b34fcf27d387835d
edmundrotimi <edmundrotimi@gmail.com>
2026-04-22 18:09:39 +0200

    add event email model config

--- event/models.py
+++ event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from contact.models import generate_custom_id
 
@@ -20,6 +21,65 @@ ATTENDANCE_CHOICES = [
     ('online', 'YouTube Live'),
 ]
 
+EVENT_CHOICES = [
+    ('ujb', 'UJ Blockchain Event'),
+    ('fews', 'CCFEWS Event'),
+]
+
+
+class EventEmailConfig(models.Model):
+    event_type = models.CharField(max_length=50, choices=EVENT_CHOICES)
+    applied_subject = models.CharField(max_length=255, default="Application Received")
+    applied_message_text = models.TextField(
+        verbose_name="Applied Message (Single)",
+        help_text="Used when saving a single record. Use HTML like &lt;br&gt; or &lt;strong&gt;. \
+            Use variables like {{ first_name }}, {{ last_name }}, {{ organization }}.",
+    )
+
+    selected_subject = models.CharField(max_length=255, default="You are Selected")
+    selected_message_text = models.TextField(
+        verbose_name="Selected Message (Single)",
+        help_text="Used when saving a single record. Use HTML like &lt;br&gt; or &lt;strong&gt;. \
+            Use variables like {{ first_name }}, {{ last_name }}, {{ organization }}.",
+    )
+    selected_bulk_message_text = models.TextField(
+        verbose_name="Selected Message (Bulk)",
+        help_text="Used when updating multiple records via Admin Actions. Do not use personal variables here. \
+            Start with a generic greeting like 'Hi there,' or 'Dear Applicant,'.",
+    )
+    rejected_subject = models.CharField(max_length=255, default="Application Update")
+    rejected_message_text = models.TextField(
+        verbose_name="Rejected Message (Single)",
+        help_text="Used when saving a single record. Use HTML like &lt;br&gt; or &lt;strong&gt;. \
+            Use variables like {{ first_name }}, {{ last_name }}, {{ organization }}.",
+    )
+    rejected_bulk_message_text = models.TextField(
+        verbose_name="Rejected Message (Bulk)",
+        help_text="Used when updating multiple records via Admin Actions. Do not use personal variables here. \
+            Start with a generic greeting like 'Hi there,' or 'Dear Applicant,'.",
+    )
+    group_link = models.URLField(
+        blank=True,
+        null=True,
+        help_text="Optional link for groups (e.g., LinkedIn/WhatsApp).",
+    )
+    attachment = models.FileField(
+        upload_to='email_flyers/',
+        blank=True,
+        null=True,
+        help_text="Optional flyer or document to attach.",
+    )
+    registration_end_time = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = "Email Configuration"
+        verbose_name_plural = "Email Configurations"
+
+    def __str__(self):
+        return f"{self.event_type.upper()} Email Template"
+
 
 class EventRegistration(models.Model):
     id = models.CharField(
@@ -31,13 +91,14 @@ class EventRegistration(models.Model):
     )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True, help_text="User's email address")
+    email = models.EmailField(help_text="User's email address")
     phone = models.CharField(max_length=20)
     faculty = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     nationality = models.CharField(max_length=100)
     year_of_study = models.CharField(max_length=20, choices=YEAR_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
+    event_type = models.CharField(max_length=50, default='FEWS', null=True, blank=True, editable=False)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
     def __str__(self):
@@ -62,6 +123,7 @@ class FewsRegistration(models.Model):
     )
     attendance_type = models.CharField(max_length=20, choices=ATTENDANCE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
+    event_type = models.CharField(max_length=50, default='UJB', null=True, blank=True, editable=False)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
     class Meta:
