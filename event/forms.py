7aa80dacb84e5cbb366b677a3ea77f639fbc7d4a
edmundrotimi <edmundrotimi@gmail.com>
2026-04-23 11:00:37 +0200

    remove email checks

--- event/forms.py
+++ event/forms.py
@@ -22,20 +22,6 @@ class EventRegistrationForm(forms.ModelForm):
             'year_of_study',
         ]
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email').lower()
-
-        # calculate the exact time 5 days ago from right now
-        five_days_ago = timezone.now() - timedelta(days=5)
-
-        # check if the email exists and was created within the last 5 days
-        if EventRegistration.objects.filter(
-            email=email, created_at__gte=five_days_ago
-        ).exists():
-            raise forms.ValidationError("This email has already been registered.")
-
-        return email
-
     def clean_phone(self):
         phone = self.cleaned_data.get('phone')
         cleaned_phone = phone.replace(" ", "").replace("+", "")
@@ -64,18 +50,6 @@ class FewsRegistrationForm(forms.ModelForm):
             'organization',
             'attendance_type',
         ]
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email').lower()
-
-        five_days_ago = timezone.now() - timedelta(days=5)
-
-        if EventRegistration.objects.filter(
-            email=email, created_at__gte=five_days_ago
-        ).exists():
-            raise forms.ValidationError("This email has already been registered.")
-
-        return email
 
     def clean_phone(self):
         phone = self.cleaned_data.get('phone')
